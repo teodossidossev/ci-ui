7fb33a37662f15f5aee98f25e3b15ebdcb059283
Teodossi Dossev <teodossi.dossev@ontotext.com>
2021-05-23 12:34:39 +0300

    Rework travis.yml and docker shell script

--- docker-push.sh
+++ docker-push.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push teodossidossev/ci-ui
+docker build -t ci-ui .
+docker images
+docker tag ci-ui "$DOCKER_USERNAME"/ci-ui
+docker push "$DOCKER_USERNAME"/ci-ui
